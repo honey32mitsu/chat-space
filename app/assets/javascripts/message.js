ab9b02b47c35cf408881c0e43105ea0e50d81e04
honey32mitsu <bright8light3master2@icloud.com>
2020-06-24 18:31:10 +0900

    修正しました

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -39,27 +39,27 @@ $(function(){
       return html;
     };
   }
-$('#new_message').on('submit', function(e){
- e.preventDefault();
- var formData = new FormData(this);
- var url = $(this).attr('action')
- $.ajax({
-   url: url,
-   type: "POST",
-   data: formData,
-   dataType: 'json',
-   processData: false,
-   contentType: false
- })
-  .done(function(data){
-    var html = buildHTML(data);
-    $('.messages').append(html);
-    $('form')[0].reset();
-    $('.message-list').animate({ scrollTop: $('.message-list')[0].scrollHeight});
-    $('.send-point').prop('disabled', false);
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('form')[0].reset();
+      $('.message-list').animate({ scrollTop: $('.message-list')[0].scrollHeight});
+      $('.send-point').prop('disabled', false);
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    });
   })
-  .fail(function() {
-    alert("メッセージ送信に失敗しました");
-});
-})
 });
